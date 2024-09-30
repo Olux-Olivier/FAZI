c1ebcd60c679c4e0d8ab111e74722cced061d69c
Olivier Kasongo <olivierkasongo539@gmail.com>
2024-09-30 11:22:26 +0200

    style commentaire

--- resources/views/pages/commentaire/index.blade.php
+++ resources/views/pages/commentaire/index.blade.php
@@ -1,37 +1,72 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FAZI | Liste Commentaires</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/home.css">
+    <title>FAZI | Accueil</title>
 </head>
 <body>
+<div class="header">
+    <img src="img/haerderImg.jpeg">
+    <div class="div-title-links">
+        <h3>FA<span>ZI</span></h3>
+        @if (Route::has('login'))
+            <nav class="-mx-3 flex flex-1 justify-end">
 
-    <h1>Tous les commentaires</h1>
+            </nav>
+        @endif
+    </div>
+    <div class="hero-bannere">
+        <h3><span>Laissez nous</span>
+            vous aider à trouvez
+            la <span>maison de vos rêves</span>
+        </h3>
+        <button>Commencer</button>
+    </div>
+</div>
 
-    @forelse($commentaires as $commentaire)
-        <strong>{{$commentaire['nom']}} {{$commentaire['prenom']}}</strong>   - <em>{{$commentaire['date']}}</em>
-        <br>
-        {{$commentaire['commentaire']}}
-        <br><br>
-        @guest
+<div class="container">
+    <div class="d-comment">
+    <h2>Tous les commentaires</h2>
 
-        @endguest
-        @auth
+        @forelse($commentaires as $commentaire)
+            <div class="d-com">
+                <div class="d-info">
+                    <h3>{{$commentaire['nom']}} {{$commentaire['prenom']}}  - <em>{{$commentaire['date']}}</em></h3>
+                </div>
+                <em><span>"</span>{{$commentaire['commentaire']}} <span>"</span></em>
+            </div>
+            @guest
 
-            @php  if(\Illuminate\Support\Facades\Auth::user()->id == $commentaire['user_id']){ @endphp
-            <form action="{{route('commentaire.destroy', $commentaire['id_commentaire'])}}" method="post">
-                @csrf
-                @method('delete')
-                <button type="submit">Supprimer</button>
-            </form>
+            @endguest
+            @auth
 
-            <a href="{{route('commentaire.edit', $commentaire['id_commentaire'])}}">Modifier le commentaire</a>
-            @php }@endphp
-        @endauth
-    @empty
-        <p>Aucun commentaire disponible.</p>
-    @endforelse
+                @php  if(\Illuminate\Support\Facades\Auth::user()->id == $commentaire['user_id']){ @endphp
+                <form action="{{route('commentaire.destroy', $commentaire['id_commentaire'])}}" method="post">
+                    @csrf
+                    @method('delete')
+                    <button type="submit">Supprimer</button>
+                </form>
 
-</body>
-</html>
+                <a href="{{route('commentaire.edit', $commentaire['id_commentaire'])}}">Modifier le commentaire</a>
+                @php }@endphp
+            @endauth
+        @empty
+            <p>Aucun commentaire disponible.</p>
+        @endforelse
+    </div>
+</div>
+<div class="footer">
+    <div class="footerIcone">
+        <h2>FA<span>ZI</span></h2>
+    </div>
+    <p>
+        Fazi est une
+        plateforme qui offre les services de
+        location et la vente des maisons
+    </p>
+</div>
+
